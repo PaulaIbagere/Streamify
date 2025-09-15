d50d96802c1c239d4ef1cb8fc917a550e3d4aaa9
Paula Ibagere <paulaibagere@gmail.com>
2025-09-15 09:53:43 +0100

    Onboarding setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,32 +9,28 @@ import CallPage from './pages/CallPage.jsx'
 import NotificationsPage from './pages/NotificationsPage.jsx'
 
 import {Toaster} from 'react-hot-toast'
-import { useQuery } from '@tanstack/react-query'
-import axios from 'axios'
-import { axiosInstance } from './lib/axios.js'
+import PageLoader from './components/PageLoader.jsx'
+import useAuthUser from './hooks/useAuthUser.js'
 
 const App = () => {
-  const {data:authData, isLoading, error} = useQuery({
-    queryKey: ["authUser"],
-    queryFn: async () => {
-      const res = await axiosInstance.get("/auth/me");
-      return res.data;
-    },
-    retry: false,
-  });
 
-  const authUser = authData?.user
+  const {isLoading, authUser} = useAuthUser();
+
+  const isAuthenticated = Boolean(authUser);
+  const isOnboarded = authUser?.isOnboarded
+  
+  if(isLoading) return <PageLoader/>;
 
   return (
     <div className='h-screen' data-theme="night">
       <Routes>
-        <Route path='/' element={authUser ? <HomePage/>: <Navigate to={"/login"}/>}/>
-        <Route path='/signup' element={!authUser ? <SignUpPage/> : <Navigate to={"/"}/>}/>
-        <Route path='/login' element={!authUser ? <LoginPage/> : <Navigate to={"/"}/>}/>
-        <Route path='/onboarding' element={authUser? <OnboardingPage/>: <Navigate to={"/login"}/>}/>
-        <Route path='/chat' element={authUser? <ChatPage/>: <Navigate to={"/login"}/>} />
-        <Route path='/call' element={authUser? <CallPage/>: <Navigate to={"/login"}/>}/>
-        <Route path='/Notification' element={authUser ?<NotificationsPage/>: <Navigate to={"/login"}/>}/>
+        <Route path='/' element={isAuthenticated && isOnboarded ? (<HomePage/>): (<Navigate to={!isAuthenticated ? "/login" : "/onboarding"}/>)}/>
+        <Route path='/signup' element={!isAuthenticated ? <SignUpPage/> : <Navigate to={"/"}/>}/>
+        <Route path='/login' element={!isAuthenticated ? <LoginPage/> : <Navigate to={"/"}/>}/>
+        <Route path='/onboarding' element={isAuthenticated? <OnboardingPage/>: <Navigate to={"/login"}/>}/>
+        <Route path='/chat' element={isAuthenticated? <ChatPage/>: <Navigate to={"/login"}/>} />
+        <Route path='/call' element={isAuthenticated? <CallPage/>: <Navigate to={"/login"}/>}/>
+        <Route path='/Notification' element={isAuthenticated ?<NotificationsPage/>: <Navigate to={"/login"}/>}/>
       </Routes>
       <Toaster/>
     </div>
--- frontend/src/pages/OnboardingPage.jsx
+++ frontend/src/pages/OnboardingPage.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import useAuthUser from "../hooks/useAuthUser"
+import { completeOnboarding } from "../lib/api";
 
 const OnboardingPage = () => {
+  const {authUser} = useAuthUser();
+  const queryClient = useQueryClient();
+
+  const [formState, setFormState] = useState({
+    fullName: authUser?.fullName || "",
+    bio: authUser?.bio || "",
+    nativeLanguage: authUser?.nativeLanguage || "",
+    learningLanguage: authUser?.learningLanguage || "",
+    location: authUser?.location || "",
+    profilePic: authUser?.profilePic || "",
+  });
+
+  const {mutate:onboardingMutation, isPending}= useMutation({
+    mutationFn: completeOnboarding,
+    onSuccess: () =>{
+      toast.success("Profile onboarded successfully")
+      queryClient.invalidateQueries({queryKey: ["authUser"]});
+    }
+  })
+
+  const handleSubmit =(e) => {
+    e.preventDefault();
+    onboardingMutation(formState);
+  }
+
   return (
-    <div>OnboardingPage</div>
+    <div className="min-h-screen bg-base-100 flex items-center justify-center p-4">
+      <div className="card bg-base-200 w-full max-w-3xl shadow-xl">
+        <div className="card-body p-6 sm:p-8">
+          <h1 className="text-2xl sm:text-3xl font-bold text-center mb-6">Complete Your Profile</h1>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
