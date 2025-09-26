8af7459d25c008797aa4755ccd6c010fdd860c11
Paula Ibagere <paulaibagere@gmail.com>
2025-09-26 16:03:16 +0100

    send friend request api created

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -42,3 +42,7 @@ export async function getOutgoingFriendReqs(){
     const response = await axiosInstance.get("/users/outgoing-friend-request")
     return response.data;
 }
+export async function sendFriendRequest(userId){
+    const response = await axiosInstance.post(`/users/friend-request/${userId}`)
+    return response.data;
+}
