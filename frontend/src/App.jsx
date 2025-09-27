88a8c70cff66f733d533fb56ba70b3378fe94931
Paula Ibagere <paulaibagere@gmail.com>
2025-09-27 22:44:58 +0100

    Chat page complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -57,7 +57,16 @@ const App = () => {
             )
           }
         />
-        <Route path='/chat' element={isAuthenticated? <ChatPage/>: <Navigate to={"/login"}/>} />
+        <Route path='/chat/:id' element={
+          isAuthenticated && isOnboarded === "true" ?(
+            <Layout showSidebar={false}>
+              <ChatPage/>
+            </Layout>
+          ): (
+            <Navigate to={!isAuthenticated ? "/login" : "/onboarding"}/>
+          )
+        } />
+
         <Route path='/call' element={isAuthenticated? <CallPage/>: <Navigate to={"/login"}/>}/>
         <Route path='/Notification' element={isAuthenticated && isOnboarded === "true"? (
           <Layout showSidebar={true}>
