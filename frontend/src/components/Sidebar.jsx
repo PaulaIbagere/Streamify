8f321fda2ce03742c22ff51699816edcc4e7d5ff
Paula Ibagere <paulaibagere@gmail.com>
2025-09-28 12:34:12 +0100

    small changes

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -27,7 +27,7 @@ const Sidebar = () => {
                 <span>Friends</span>
             </Link>            
             
-            <Link to="/notifications" className={`btn btn-ghost justify-start w-full gap-3 px-3 normal-case ${currentPath === "/notifications" ? "btn-active": ""}`}>
+            <Link to="/notification" className={`btn btn-ghost justify-start w-full gap-3 px-3 normal-case ${currentPath === "/notifications" ? "btn-active": ""}`}>
                 <BellIcon className="size-5 text-base-content opacity-70"/>
                 <span>Notification</span>
             </Link>
